5732e30bf2b9d6c14a7f2005385845da713f618a
SamiBeig <Sami.Beig45@myhunter.cuny.edu>
2020-03-25 13:01:14 -0400

    imported EPSignature successfully

--- SignatureTest/ViewController.swift
+++ SignatureTest/ViewController.swift
@@ -8,74 +8,32 @@
 
 import UIKit
 import SwiftSignatureView
-import EPSignatureView
+import EPSignature
 
-/*
-class Canvas: UIView {
-    
-    override func draw(_ rect: CGRect) {
-        super.draw(rect)
-        
-        guard let context = UIGraphicsGetCurrentContext() else { return }
-
-        context.setStrokeColor(UIColor.red.cgColor)
-        context.setLineWidth(10)
-        context.setLineCap(.butt)
-        
-        lines.forEach { (line) in
-            for (i, p) in line.enumerated() {
-                if i == 0 {
-                    context.move(to: p)
-                } else {
-                    context.addLine(to: p)
-                }
-            }
-        }
-        
-        context.strokePath()
-        
-    }
-
-    var lines = [[CGPoint]]()
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        lines.append([CGPoint]())
-    }
-    
-    // track the finger as we move across screen
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let point = touches.first?.location(in: nil) else { return }
-        
-        guard var lastLine = lines.popLast() else { return }
-        lastLine.append(point)
-        lines.append(lastLine)
-        setNeedsDisplay()
-    }
-    
-}
- 
-*/
 
 class ViewController: UIViewController {
-    
-  @IBOutlet weak var signatureView: UIView!
+      
+  @IBOutlet weak var signatureView: EPSignatureView!
   
-  //let canvas = Canvas()
-
     override func viewDidLoad() {
         super.viewDidLoad()
       
+      //Initializing EPSignature
       /*
-      view.addSubview(canvas)
-      canvas.backgroundColor = .white
-      canvas.frame = view.frame
-      */
       let signatureVC = EPSignatureViewController(signatureDelegate: self, showsDate: true, showsSaveSignatureOption: true)
       signatureVC.subtitleText = "I agree to the terms and conditions"
       signatureVC.title = "John Doe"
       let nav = UINavigationController(rootViewController: signatureVC)
       presentViewController(nav, animated: true, completion: nil)
-        
+       */
+      
+      
+       
+       
+       
+       
+      
+    
     }
 
 }
